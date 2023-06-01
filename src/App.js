aae8b0b66c3ef7a4447d8b93cf65c9723cfd541f
Alen Bolatov <bolatovalen@gmail.com>
2023-06-02 01:02:39 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>sadfsadd</div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js",
+    },
+    devServer: {
+        port: 3010,
+        static: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    },
+};
